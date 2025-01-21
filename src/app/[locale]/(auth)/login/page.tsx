d4ba8e9ec8fbfce13963d4e545ce0bb738c31196
Vladimir Mysko <57433511+vladimirmysko@users.noreply.github.com>
2025-01-21 14:19:32 +0500

    Add Radix UI themes and implement login page layout with redirection

---
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import { Flex, Heading } from '@radix-ui/themes';
+
+export default function Page() {
+  return (
+    <Flex>
+      <Heading>Login page</Heading>
+    </Flex>
+  );
+}
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,18 +1,17 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import { clsx } from 'clsx';
 import { getScopedI18n } from '@/locales/server';
 
 import { I18nProviderClient } from '@/locales/client';
+import { Theme } from '@radix-ui/themes';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const inter = Inter({
+  subsets: ['latin', 'cyrillic'],
+  display: 'swap',
+  variable: '--font-inter',
+  weight: 'variable',
+  axes: ['opsz'],
 });
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -34,12 +33,19 @@ export default async function LocaleLayout({
   const { locale } = await params;
 
   return (
-    <html
-      lang={locale}
-      className={clsx(geistSans.variable, geistMono.variable)}
-    >
+    <html lang={locale} className={clsx(inter.variable)}>
       <body style={{ textRendering: 'optimizeLegibility' }}>
-        <I18nProviderClient locale={locale}>{children}</I18nProviderClient>
+        <I18nProviderClient locale={locale}>
+          <Theme
+            accentColor="teal"
+            grayColor="gray"
+            radius="medium"
+            scaling="100%"
+            panelBackground="solid"
+          >
+            {children}
+          </Theme>
+        </I18nProviderClient>
       </body>
     </html>
   );
