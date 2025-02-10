6444fa4927f2928785de844fb8d91c611bb94ac3
Vladimir Mysko <57433511+vladimirmysko@users.noreply.github.com>
2025-02-10 16:54:36 +0500

    Add internationalization support and initial locale files

---
+++ prettier.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import("prettier").Options} */
+const config = {
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+  trailingComma: 'es5',
+  proseWrap: 'always',
+  printWidth: 80,
+  arrowParens: 'always',
+  arrowParens: 'always',
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  embeddedLanguageFormatting: 'auto',
+};
+
+export default config;
